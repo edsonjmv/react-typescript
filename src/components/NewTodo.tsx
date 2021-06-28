024842beeee4a20d43ee11894fe8fc767695f8fb
Edson Machado <edsonjmv@gmail.com>
2021-06-28 17:46:47 +0200

    form submissions & useRef

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from 'react';
+
+const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="todo-text">Todo text</label>
+      <input id="todo-text" type="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
